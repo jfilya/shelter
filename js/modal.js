cc6044522960f52d83a376098b27c2974e781b00
jfilya <julia.94@bk.ru>
2022-04-18 18:24:41 +0300

    feat: array of pets rendered separately

---
+++ js/modal.js
@@ -0,0 +1,87 @@
+const modals = document.querySelector('.modal');
+const overlays = document.querySelector('.overlay');
+
+let modalWindow = document.querySelector('.modal');
+let modaling = pets.slice();
+for (let mod of modaling) {
+  let block = document.createElement('div');
+  modalWindow.appendChild(block);
+  block.className =`modal-${mod.name}`
+
+  let btnClose = document.createElement('button')
+  block.appendChild(btnClose);
+
+
+  btnClose.className=`closeModalBtn close-${mod.name}`
+  let imgClose = document.createElement('img')
+  imgClose.src = "img/CloseModal.svg"
+  imgClose.alt="close"
+  btnClose.appendChild(imgClose);
+  btnClose.addEventListener('click', () => {
+    overlays.classList.remove('activeOverlay');
+    modals.classList.remove('activeModal');
+    block.classList.remove('modal-pets');
+  })
+
+
+  let imgModal = document.createElement('img')
+  imgModal.src = mod.img;
+  block.appendChild(imgModal);
+  imgModal.alt=`pets-${mod.name}`
+  imgModal.className="imgPets"
+
+  let content = document.createElement('div');
+  block.appendChild(content);
+  content.className="content"
+
+  let hName = document.createElement('h3');
+  hName.innerHTML = mod.name  
+  hName.className = "content__title"
+  content.appendChild(hName)
+
+  let hPet = document.createElement('h5');
+  hPet.innerHTML =` ${mod.type} - ${mod.breed}`
+
+  hPet.className = "content__titleMini"
+  content.appendChild(hPet)
+
+  let text = document.createElement('p');
+  text.className = "content__info"
+  text.innerHTML = mod.description
+  content.appendChild(text)
+
+  let contentList = document.createElement('ul');
+  contentList.className = "content__list"
+  content.appendChild(contentList)
+
+  let contentListItem1 = document.createElement('li');  
+  contentListItem1.innerHTML = `Age: ` 
+  let itemList1 =  document.createElement('span') 
+  itemList1.innerHTML = mod.age 
+  contentListItem1.appendChild(itemList1)     
+  contentList.appendChild(contentListItem1)
+
+
+  let contentListItem2 = document.createElement('li');    
+  contentListItem2.innerHTML = "Inoculations: "
+  let itemList2 =  document.createElement('span') 
+  itemList2.innerHTML = mod.inoculations 
+  contentListItem2.appendChild(itemList2) 
+  contentList.appendChild(contentListItem2)
+
+  let contentListItem3 = document.createElement('li');    
+  contentListItem3.innerHTML = "Diseases: "
+  let itemList3 =  document.createElement('span') 
+  itemList3.innerHTML = mod.diseases 
+  contentListItem3.appendChild(itemList3)
+  contentList.appendChild(contentListItem3)
+
+  let contentListItem4 = document.createElement('li');    
+  contentListItem4.innerHTML = "Parasites: "
+  let itemList4 =  document.createElement('span') 
+  itemList4.innerHTML = mod.parasites 
+  contentListItem4.appendChild(itemList4)
+  contentList.appendChild(contentListItem4)
+ 
+}
+
--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,49 @@
+let base = document.querySelector('#base');
+let notes = pets.slice();
+
+const modalsMain = document.querySelector('.modal');
+const overlaysMain = document.querySelector('.overlay');
+
+for (let note of notes) {
+  let item = document.createElement('div');
+  base.appendChild(item);
+  item.className = "item"
+  
+  let tr = document.createElement('div');
+  item.appendChild(tr);
+  tr.className = `pets__pet pets__item btn-${note.name}`
+  tr.addEventListener('click', () => {
+    overlaysMain.classList.add('activeOverlay');
+    modalsMain.classList.add('activeModal');
+    document.querySelector(`.modal-${note.name}`).classList.add('modal-pets');
+  });
+
+  const closeModels = () => {
+    overlaysMain.classList.remove('activeOverlay');
+    document.querySelector(`.modal-${note.name}`).classList.remove('modal-pets');
+  };
+  overlaysMain.addEventListener('click', closeModels);
+
+  let img = document.createElement('img');
+  img.src= note.img;
+  img.alt = `pets-${note.name}`
+  tr.appendChild(img);
+  img.className = "pets__pet__image"
+
+  let h = document.createElement('h5');   
+  h.innerHTML= note.name;
+  tr.appendChild(h);
+  h.className = "pets__pet__title";
+
+  let btn = document.createElement('div');   
+  btn.innerHTML= 'Learn more';
+  tr.appendChild(btn);
+  btn.className = "btn btnLearn pets__pet__btn ";
+  
+
+}
+
+
 let slides = document.querySelectorAll('.item');//каждая карточка слайда
 let leftButton = document.querySelector('.prev');
 let rightButton = document.querySelector('.next');
