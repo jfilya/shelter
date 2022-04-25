b630814b347b6003037d47e197f00b3bea7decef
Julia Filippova <julia.94@bk.ru>
2022-04-25 03:13:17 +0300

    fix: correct resize slider

--- js/slider.js
+++ js/slider.js
@@ -10,65 +10,83 @@ function mix(arr) {
     .sort()
     .map((i) => i[1]);
 }
+rez();
 
-let notesSlider=[];
-notesSlider.push(...mix(pets))
-let notes = notesSlider.splice(0,3);
-function showItem() {
+window.addEventListener("resize", rez);
 
-  base.innerHTML = "";
-  for (let note of notes) {
-    let item = document.createElement("div");
-
-    base.appendChild(item);
-    item.className = "item";
-
-    let tr = document.createElement("div");
-    item.appendChild(tr);
-    tr.className = `pets__pet pets__item btn-${note.name}`;
-    tr.addEventListener("click", () => {
-      overlaysMain.classList.add("activeOverlay");
-      modalsMain.classList.add("activeModal");
-      document.querySelector(`.modal-${note.name}`).classList.add("modal-pets");
-    });
-
-    const closeModels = () => {
-      overlaysMain.classList.remove("activeOverlay");
-      document
-        .querySelector(`.modal-${note.name}`)
-        .classList.remove("modal-pets");
-    };
-    overlaysMain.addEventListener("click", closeModels);
-
-    let img = document.createElement("img");
-    img.src = note.img;
-    img.alt = `pets-${note.name}`;
-    tr.appendChild(img);
-    img.className = "pets__pet__image";
-
-    let h = document.createElement("h5");
-    h.innerHTML = note.name;
-    tr.appendChild(h);
-    h.className = "pets__pet__title";
-
-    let btn = document.createElement("div");
-    btn.innerHTML = "Learn more";
-    tr.appendChild(btn);
-    btn.className = "btn btnLearn pets__pet__btn ";
+function rez() {
+  if (window.innerWidth >= 1280) {
+    notesOnItem = 3;
   }
+  if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+    notesOnItem = 2;
+  }
+  if (window.innerWidth >= 320 && window.innerWidth < 768) {
+    notesOnItem = 1;
+  }
+
+  leftButton.onclick = () => {
+    notesSlider = notesSlider.concat(...notes);
+    notes = notesSlider.splice(0, notesOnItem);
+    notesSlider = mix(notesSlider);
+    showItem(notes);
+  };
+  rightButton.onclick = () => {
+    notesSlider = notesSlider.concat(...notes);
+    notes = notesSlider.splice(0, notesOnItem);
+    notesSlider = mix(notesSlider);
+    showItem(notes);
+  };
+
+  let notesSlider = [];
+  notesSlider.push(...mix(pets));
+  let notes = notesSlider.splice(0, notesOnItem);
+  function showItem() {
+    base.innerHTML = "";
+    for (let note of notes) {
+      let item = document.createElement("div");
+
+      base.appendChild(item);
+      item.className = "item";
+
+      let tr = document.createElement("div");
+      item.appendChild(tr);
+      tr.className = `pets__pet pets__item btn-${note.name}`;
+      tr.addEventListener("click", () => {
+        overlaysMain.classList.add("activeOverlay");
+        modalsMain.classList.add("activeModal");
+        document
+          .querySelector(`.modal-${note.name}`)
+          .classList.add("modal-pets");
+        document.querySelector("body").classList.add("scroll");
+      });
+
+      const closeModels = () => {
+        overlaysMain.classList.remove("activeOverlay");
+        document
+          .querySelector(`.modal-${note.name}`)
+          .classList.remove("modal-pets");
+        document.querySelector("body").classList.remove("scroll");
+      };
+      overlaysMain.addEventListener("click", closeModels);
+
+      let img = document.createElement("img");
+      img.src = note.img;
+      img.alt = `pets-${note.name}`;
+      tr.appendChild(img);
+      img.className = "pets__pet__image";
+
+      let h = document.createElement("h5");
+      h.innerHTML = note.name;
+      tr.appendChild(h);
+      h.className = "pets__pet__title";
+
+      let btn = document.createElement("div");
+      btn.innerHTML = "Learn more";
+      tr.appendChild(btn);
+      btn.className = "btn btnLearn pets__pet__btn ";
+    }
+  }
+
+  showItem(0);
 }
-
-leftButton.onclick = () => {
-  notesSlider = notesSlider.concat(...notes)
-  notes = notesSlider.splice(0,3);
-  notesSlider=mix(notesSlider)
-  showItem(notes);
-};
-rightButton.onclick = () => {
-  notesSlider = notesSlider.concat(...notes)
-  notes = notesSlider.splice(0,3);
-  notesSlider=mix(notesSlider)
-  showItem(notes);
-};
-
-showItem(0);
